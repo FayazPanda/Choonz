562404f6df9578b9c5b686a9edc6c3afe4a051f4
Usama <umalik@academytrainee.com>
2021-01-18 12:58:15 +0000

    UM: Created view all album page

---
+++ target/classes/static/js/album.js
@@ -0,0 +1,45 @@
+function getAlbums() {
+    fetch('http://localhost:8082/albums/read')
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
+                // Examine the text in the response
+                response.json().then(function (albumData) {
+                    console.log(albumData)
+
+                    let row = document.getElementById("albumPlaceHolder");
+                    row.innerHTML = '';
+                    //let data = Object.keys(listData[0]);
+
+                    for (let album of albumData) {
+                        //globalTaskList.push(list["items"]);
+                        row.insertAdjacentHTML("beforeend", albumCard(album["name"]));
+                    }
+                });
+            }
+        )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
+
+function albumCard(title) {
+    return '<div class="col-md">\
+    <div class="card">\
+        <img src="https://www.webfx.com/blog/images/cdn.designinstruct.com/files/582-how-to-image-placeholders/generic-image-placeholder.png"\
+            class="card-img-top" alt="...">\
+        <div class="card-body">\
+            <a href="#" class="stretched-link">\
+                <h5 class="card-title">'+ title +'</h5>\
+                <p class="card-text">Some quick example text to build on the album and some artist / track data</p>\
+            </a>\
+        </div>\
+    </div>\
+</div>'
+}
+
+getAlbums();
