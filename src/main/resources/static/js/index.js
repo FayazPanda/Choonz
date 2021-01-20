99ea9e758229cad09cb033feb73d33089f5d4ded
Usama <umalik@academytrainee.com>
2021-01-20 10:54:46 +0000

    UM: added playlist functionality to the homepage

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -34,6 +34,42 @@ function getPopAlbums() {
         });
 }
 
+function getPopPlaylist() {
+    fetch('http://localhost:8082/playlists/read')
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
+                // Examine the text in the response
+                response.json().then(function (playlistData) {
+                    console.log(playlistData)
+
+                    let gallery = document.getElementById("popPlaylist");
+                    gallery.innerHTML = '';
+
+                    let playlistSize = Object.keys(playlistData).length;
+                    console.log(playlistSize);
+                    for (let i = 0; i < playlistSize; i++) {
+                        console.log(i);
+                        if(i < 5){
+                            console.log(playlistData[i]);
+                            gallery.insertAdjacentHTML("beforeend", playlistCard(playlistData[i]["id"],playlistData[i]["name"], playlistData[i]["user"]["username"]));
+                        }
+                        else{
+                            break
+                        }
+                    }
+                });
+            }
+        )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
+
 function albumCard(id, title, artist) {
     return '<div class="tile">\
     <a href="/album.html?id='+id+'">\
@@ -44,4 +80,15 @@ function albumCard(id, title, artist) {
 </div>'
 }
 
+function playlistCard(id, title, artist) {
+    return '<div class="tile">\
+    <a href="/playlist.html?id='+id+'">\
+    <img src="img/album/alterbridge.png">\
+    <h3>'+title+'</h3>\
+    <p>By: '+artist+'</p>\
+    </a>\
+</div>'
+}
+
 getPopAlbums();
+getPopPlaylist();
