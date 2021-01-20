8d9dc001143b17618cc5eecb97eec414043cbfa7
harryfrescoQA <hfresco@qa.com>
2021-01-20 16:57:11 +0000

    HF: Added form

--- src/main/resources/static/js/readCookie.js
+++ src/main/resources/static/js/readCookie.js
@@ -1,3 +1,10 @@
 let cookie = document.cookie;
 
+function userId(){
+   let userID=cookie
+  .split('; ')
+  .find(row => row.startsWith('userid'))
+  .split('=')[1];
+  return userID;
+}
 console.log(cookie);
