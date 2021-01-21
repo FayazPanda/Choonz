08ebb9ad7970c6201bddec82d02c22db19d1193b
harryfrescoQA <hfresco@qa.com>
2021-01-21 12:39:04 +0000

    HF: Added browse playlists

--- src/main/resources/static/js/browse.js
+++ src/main/resources/static/js/browse.js
@@ -47,8 +47,8 @@ function getAlbums() {
                 // Examine the text in the response
                 response.json().then(function (data) {
                     console.log(data)
-                    let artistName = document.getElementById("name");
-                    artistName.innerHTML = "Artist: " + data["name"];
+                    let albumName = document.getElementById("name");
+                    albumName.innerHTML = "All Albums";
 
                     let gallery = document.getElementById("all");
                     gallery.innerHTML = '';
@@ -101,7 +101,7 @@ function getArtist() {
             console.log('Fetch Error :-S', err);
         });
 }
-function artistCard(id, title, artist, cover) {
+function artistCard(id, title) {
     return '<div class="tile">\
     <a href="/artist.html?id='+id+'">\
     <h3>'+title+'</h3>\
@@ -109,6 +109,44 @@ function artistCard(id, title, artist, cover) {
 </div>'
 }
 
+function getPlaylists() {
+    fetch('http://localhost:8082/playlists/read/')
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
+                // Examine the text in the response
+                response.json().then(function (data) {
+                    console.log(data)
+                    let playlist = document.getElementById("name");
+                    playlist.innerHTML = "All Playlists";
+
+                    let gallery = document.getElementById("all");
+                    gallery.innerHTML = '';
+
+                    for (let playlist of data) {
+                        gallery.insertAdjacentHTML("beforeend", playlistCard(playlist["id"], playlist["name"], playlist["artwork"], playlist.user["username"]));
+                    }
+                });
+            }
+        )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
+
+function playlistCard(id, title, cover, user) {
+    return '<div class="tile">\
+    <a href="/playlist.html?id='+id+'">\
+    <img id="albumCover" src="'+cover+'">\
+    <h3>'+title+'</h3>\
+    <p>By: '+ user +'</p>\
+    </a>\
+</div>'
+}
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const page = urlParams.get('page');
@@ -124,7 +162,7 @@ else if(page=="artists"){
     getArtist();
 }
 else if(page=="playlists"){
-
+    getPlaylists();
 }
 else if(page=="myplaylists"){
 
