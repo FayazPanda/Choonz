55aeaef6c23572145e2c756d95c6cb836e52118a
Usama <umalik@academytrainee.com>
2021-01-28 15:38:47 +0000

    UM: linked up genre

--- src/main/resources/static/js/genre.js
+++ src/main/resources/static/js/genre.js
@@ -1,3 +1,6 @@
+var genreTitle = "";
+var genreDesc = "";
+
 function getGenre(id) {
     fetch('http://localhost:8082/genres/read/' + id)
         .then(
@@ -12,6 +15,9 @@ function getGenre(id) {
                     console.log(genreData)
                     let genreName = document.getElementById("genreName");
                     genreName.innerHTML = "Genre: " + genreData["name"];
+
+                    genreTitle = genreData["name"];
+                    genreDesc = genreData["description"];
 
                     let gallery = document.getElementById("allAlbums");
                     gallery.innerHTML = '';
@@ -53,6 +59,23 @@ function getAlbumArtist(id) {
         });
 }
 
+function putGenre(id, data) {
+    fetch('http://localhost:8082/genres/update/' + id, {
+        method: 'put', //post, put,delete
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify(data)
+    })
+        .then(function (data) {
+            console.log('Request succeeded with JSON response', data);
+            //fillPage();
+        })
+        .catch(function (error) {
+            console.log('Request failed', error);
+        })
+}
+
 function albumCard(id, title, cover) {
     return '<div class="tile">\
     <a href="/album.html?id=' + id + '">\
@@ -70,31 +93,21 @@ console.log(id);
 
 getGenre(id);
 
+$('#editGenreModal').on('show.bs.modal', function (e) {
+    document.getElementById("genre-title").value = genreTitle;
+    document.getElementById("desc").value = genreDesc;
+    // document.getElementById("genreListSelect").value = selectGenre;
+    // console.log(selectGenre);
+})
+
 $(document).on("click", "#saveEditBtn", function () {
     //let playlistId = document.getElementById("listSelect").value;
     //console.log("Add " + trackToAdd + " " + playlistId);
     data = {
-        "title": document.getElementById("genre-title").value,
+        "name": document.getElementById("genre-title").value,
         "description": document.getElementById("desc").value
     }
     putGenre(id, data);
 
-    //location.reload();
+    location.reload();
 });
-
-function putGenre(id, data) {
-    fetch('http://localhost:8082/genres/update/' + id, {
-        method: 'put', //post, put,delete
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        },
-        body: JSON.stringify(data)
-    })
-        .then(function (data) {
-            console.log('Request succeeded with JSON response', data);
-            //fillPage();
-        })
-        .catch(function (error) {
-            console.log('Request failed', error);
-        })
-}
